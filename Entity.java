8815d35caa24c7a868b510df6bc5bd74c67a7326
WhatsitTooya <adamlhancock@yahoo.com>
2016-05-11 16:37:28 -0700

    Working on Entity
    Making the constructor

--- Entity.java
+++ Entity.java
@@ -3,6 +3,15 @@ public class Entity{
 	public String name; 
 	public ArrayList<Opinion> views; //a list of opinions
 	
-
-
+	public Entity (String name, ArrayList<Opinion> views) {
+		this.name = name;
+		this.views = views;
+		for (int i = 0; i < this.views.size(); i++) {
+			
+		}
+	}
+	
+	public static int random(Random rand,int num1,int num2){  //returns a random value between num1 and num2 (inclusive)
+		return(num1 -1 + (int)Math.ceil(rand.nextDouble()*(num2-num1+1)));
+	}
 }
