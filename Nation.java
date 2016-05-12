ab74274a2a7ef643947534467ec12dbbea127e04
NamesAreAPain <username@users.noreply.github.com>
2016-05-11 17:49:54 -0700

    Election Procedures

--- Nation.java
+++ Nation.java
@@ -4,6 +4,9 @@ public class Nation{
 	public Party[] parties;
 	public Issue[] issues;
 	
+	public HashMap<Party,Integer> previousElectionResults = null;
+	public Party[] viable;
+	
 	public Nation(int population, int partyCount, int issueCount){
 		this.populace = new Voter[population];
 		this.parties = new Party[partyCount];
@@ -21,6 +24,47 @@ public class Nation{
 			parties[i] = new Party();
 		}	
 				
+	}
+	
+	public Party runElection(){ //runs an election, selects winner;
+
+		ArrayList<Party> votes = new ArrayList<Party>();
+		
+		for(Voter x : populace){
+			votes.add(x.vote());
+		}
+		
+		
+		HashMap<Party,Integer> results = new HashMap<Party,Integer>();
+		
+		for(Party s: votes) { //populates HashMap 
+			Integer c = occurences.get(s); //how many times has the word been seen before?
+			if(c == null) c = new Integer(0); //if zero, avoids null case
+			c++; //adds to the total
+			occurences.put(s,c); //maps it.
+		}
+		
+		
+		Integer a = new Integer(population); //starts with total words in book
+		
+		while(!occurences.containsValue(a)){ //finds the most common words
+			a--;
+		}
+		
+		ArrayList<String> uniqa = new ArrayList<String>(); //ARRAYLIST OF STRINGS 
+		while(a > -1){  
+			for(String x: book){ 
+				if(occurences.get(x).equals(a) && occurences.get(x) > .05 * population ){
+					uniqa.add(x);
+				}
+			}
+			a--;
+		}
+		
+		previousElectionResults = occurences;
+		viable = uniqa.toArray(new Party[0]);
+		
+		
 	}
 	
 }
