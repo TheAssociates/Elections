d963288ab73bbd9b03a171253bcfc1e0155b986b
WhatsitTooya <adamlhancock@yahoo.com>
2016-05-11 19:02:13 -0700

    Adam did things

--- Entity.java
+++ Entity.java
@@ -11,7 +11,7 @@ public class Entity{
 		}
 	}
 	
-	public static int random(Random rand,int num1,int num2){  //returns a random value between num1 and num2 (inclusive)
+	public int random(Random rand,int num1,int num2){  //returns a random value between num1 and num2 (inclusive)
 		return(num1 -1 + (int)Math.ceil(rand.nextDouble()*(num2-num1+1)));
 	}
 }
