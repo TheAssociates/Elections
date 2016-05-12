9f6df4a9d01058cd0d73af21e5f00ad50d453cf7
WhatsitTooya <adamlhancock@yahoo.com>
2016-05-11 17:12:56 -0700

    Entity has a Constructor
    Constructor is good to go

--- Entity.java
+++ Entity.java
@@ -3,20 +3,15 @@ public class Entity{
 	public String name; 
 	public ArrayList<Opinion> views; //a list of opinions
 	
-<<<<<<< HEAD
-	public Entity (String name, ArrayList<Opinion> views) {
+	public Entity (String name, Issue[] views) {
 		this.name = name;
-		this.views = views;
-		for (int i = 0; i < this.views.size(); i++) {
-			
+		Random rand = new Random();
+		for (int i = 0; i < views.length; i++) {
+			this.views.add(new Opinion(views[i], random(rand, -100, 100), random(rand, 1, 100)));
 		}
 	}
 	
 	public static int random(Random rand,int num1,int num2){  //returns a random value between num1 and num2 (inclusive)
 		return(num1 -1 + (int)Math.ceil(rand.nextDouble()*(num2-num1+1)));
 	}
-=======
-	
-	
->>>>>>> origin/master
 }
