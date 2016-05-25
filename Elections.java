c25fb826623df8c7db7e2cdd8738dea3e3f654ea
NamesAreAPain <username@users.noreply.github.com>
2016-05-24 18:49:11 -0700

    Ends if 5 consecutive wins

--- Elections.java
+++ Elections.java
@@ -8,12 +8,17 @@ public class Elections{
 	public static int POPULATION;
 	public static int PARTYCOUNT;
 	public static int ISSUECOUNT;
-	public static final Random SUPER_RAND = new Random(3141592);
+	public static final Random SUPER_SUPER_RAND = new Random();
+	public static final long SEED = SUPER_SUPER_RAND.nextLong();
+	public static final Random SUPER_RAND = new Random(SEED);
 	
 	public static final Party[] typecast = new Party[0];
 	
 	
 	public static void main(String[]arg){
+		
+		System.out.println(SEED);
+		
 		
 		POPULATION = Integer.parseInt(arg[0]);
 		PARTYCOUNT = Integer.parseInt(arg[1]);
@@ -21,11 +26,15 @@ public class Elections{
 		
 		Nation natl = new Nation(POPULATION, PARTYCOUNT, ISSUECOUNT);
 		
+		int reign = 0;
+		Party lastWinner = null;
 		
-		
-		while(natl.viable == null || natl.viable.length > 1){
+		while((natl.viable == null || natl.viable.length > 1) && reign < 5){
 			int totVotes = 0;
 			Party winner = natl.runElection();
+			if(winner == lastWinner) reign++;
+			lastWinner = winner;
+			
 			System.out.println(natl.viable.length );
 			for(Party x : natl.parties){
 				int votes =  (natl.previousElectionResults.get(x) == null) ? 0 : natl.previousElectionResults.get(x); 
